14b3cdfccb99d5f366a89f99e05f4242ba8a0d21
Colin Geniet <colin.geniet@gmail.com>
2016-11-14 14:30:55 +0100

    cleaned up mess in class form
    in class form : deleted remove and find methods (no use), deleted duplicate test in add method (duplicate are not critical), deleted constructor from array (probably not usefull, will re-add if required), deleted updatebox method (unused with those changes)
    added clear method

--- form.cpp
+++ form.cpp
@@ -2,31 +2,20 @@
 
 
 Form::Form() :
-    array_size(FORM_DEFAULT_SIZE),
-    n_squares(0),
-    squares(new Point[array_size]) {}
+    capacity(FORM_DEFAULT_SIZE),
+    size(0),
+    squares(new Point[capacity]) {}
 
 Form::Form(const Form& f) :
-    array_size(f.n_squares),
-    n_squares(f.n_squares),
-    squares(new Point[array_size]),
+    capacity(f.size),
+    size(f.size),
+    squares(new Point[capacity]),
     boxmin(f.boxmin),
     boxmax(f.boxmax)
 {
-    for(size_t i=0; i<n_squares; i++) {
+    for(size_t i=0; i<size; i++) {
         squares[i] = f.squares[i];
     }
-}
-
-Form::Form(const Point* T, size_t n) :
-    array_size(n),
-    n_squares(n),
-    squares(new Point[array_size])
-{
-    for(size_t i=0; i<n_squares; i++) {
-        squares[i] = T[i];
-    }
-    updatebox();
 }
 
 
@@ -39,14 +28,14 @@ Form::~Form()
 Form& Form::operator=(const Form& f)
 {
     if(&f != this) {
-        if(array_size < f.n_squares) {
+        if(capacity < f.size) {
             delete[] squares;
-            array_size = f.n_squares;
-            squares = new Point[array_size];
+            capacity = f.size;
+            squares = new Point[capacity];
         }
-        n_squares = f.n_squares;
+        size = f.size;
 
-        for(size_t i=0; i<n_squares; i++) {
+        for(size_t i=0; i<size; i++) {
             squares[i] = f.squares[i];
         }
         boxmin = f.boxmin;
@@ -57,24 +46,14 @@ Form& Form::operator=(const Form& f)
 
 
 
-void Form::updatebox()
+void Form::clear()
 {
-    if(n_squares == 0) {
-        boxmin.x = 0; boxmin.y = -1;
-        boxmax.x = 0; boxmax.y = -1;
-    } else {
-        boxmin.x = squares[0].x, boxmax.x = squares[0].x;
-        boxmin.y = squares[0].y, boxmax.y = squares[0].y;
-
-        for(size_t i=1; i<n_squares; i++) {
-            if(squares[i].x < boxmin.x) boxmin.x = squares[i].x;
-            if(squares[i].x > boxmax.x) boxmax.x = squares[i].x;
-            if(squares[i].y < boxmin.y) boxmin.y = squares[i].y;
-            if(squares[i].y > boxmax.y) boxmax.y = squares[i].y;
-        }
-    }
+    size = 0;
+    boxmin.x = 0;
+    boxmin.y = 0;
+    boxmax.x = 0;
+    boxmax.y = 0;
 }
-
 
 void Form::add(Point p)
 {
@@ -82,48 +61,31 @@ void Form::add(Point p)
 }
 
 void Form::add(int x, int y) {
-    if(find(x, y) == (size_t) -1) {
-        if(n_squares == array_size) {
-            resize();
-        }
-        squares[n_squares].x = x;
-        squares[n_squares].y = y;
-        n_squares++;
+    if(size == capacity) {
+        resize();
+    }
+    squares[size].x = x;
+    squares[size].y = y;
+    size++;
 
-        if(n_squares == 1) {
-            boxmin.x = boxmax.x = x;
-            boxmin.y = boxmax.y = y;
-        } else {
-            if(x < boxmin.x) boxmin.x = x;
-            if(x > boxmax.x) boxmax.x = x;
-            if(y < boxmin.y) boxmin.y = y;
-            if(y > boxmax.y) boxmax.y = y;
-        }
+    if(size == 1) {
+        boxmin.x = boxmax.x = x;
+        boxmin.y = boxmax.y = y;
+    } else {
+        if(x < boxmin.x) boxmin.x = x;
+        if(x > boxmax.x) boxmax.x = x;
+        if(y < boxmin.y) boxmin.y = y;
+        if(y > boxmax.y) boxmax.y = y;
     }
 }
 
 
-void Form::remove(Point p)
+size_t Form::size() const
 {
-    remove(p.x, p.y);
+    return size;
 }
 
-void Form::remove(int x, int y)
-{
-    size_t i = find(x, y);
-    if(i != (size_t) -1) {
-        squares[i] = squares[--n_squares];
-        updatebox();
-    }
-}
-
-
-size_t Form::getsize() const
-{
-    return n_squares;
-}
-
-const Point& Form::operator[](size_t n) const
+Point Form::operator[](size_t n) const
 {
     return squares[n];
 }
@@ -139,23 +101,12 @@ Point Form::getboxmax() const
 }
 
 
-
-size_t Form::find(int x, int y) const
-{
-    for(size_t i=0; i<n_squares; i++) {
-        if(squares[i].x == x && squares[i].y == y) {
-            return i;
-        }
-    }
-    return (size_t) -1;
-}
-
 void Form::resize()
 {
-    array_size = array_size == 0 ? FORM_DEFAULT_SIZE : array_size*2;
-    Point *tmp = new Point[array_size];
+    capacity = capacity == 0 ? FORM_DEFAULT_SIZE : capacity*2;
+    Point *tmp = new Point[capacity];
 
-    for(size_t i=0; i<n_squares; i++) {
+    for(size_t i=0; i<size; i++) {
         tmp[i] = squares[i];
     }
 
--- form.h
+++ form.h
@@ -12,10 +12,7 @@ struct Point
 
 /* This class stores forms made of squares and keeps it's boundary box,
  * ie the smallest rectangle that contains the form */
-/* Implementation a faire : /!\ tableau dynamique, faire constructeurs /
- * destructeur / operator= correctement
- * Toujours s'assurer que boxmin et boxmax sont mis a jour
- */
+
 #define FORM_DEFAULT_SIZE 5
 
 class Form
@@ -23,36 +20,27 @@ class Form
 public:
     Form();
     Form(const Form&);
-    Form(const Point*, size_t);
 
     ~Form();
 
     Form& operator= (const Form&);
 
-    // recalculate the boundary box.
-    void updatebox();
+    void clear();
 
-    // add a square (if it does not already exist)
+    // add a square
     void add(Point);
     void add(int, int);
 
-    // remove a square (if it exists)
-    void remove(Point);
-    void remove(int, int);
-
     // accessors
-    size_t getsize() const;
-    const Point& operator[](size_t) const;
+    size_t size() const;
+    Point operator[](size_t) const;
     Point getboxmin() const;
     Point getboxmax() const;
 
 private:
-    size_t array_size, n_squares;
+    size_t size, capacity;
     Point *squares;
     Point boxmin, boxmax;
-
-    // find a square, return its index, -1 if it does not exist
-    size_t find(int, int) const;
 
     void resize();
 };
