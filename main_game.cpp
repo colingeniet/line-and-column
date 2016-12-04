90c6ee63e243bc4bb72e76689bdf8f7bab0da6d6
Colin Geniet <colin.geniet@gmail.com>
2016-12-04 17:10:06 +0100

    improved random selection
    the same form wont be selected twice if at all possible

--- main_game.cpp
+++ main_game.cpp
@@ -117,7 +117,6 @@ bool mainGame::add_form_to_set(const Form &form, int color)
 }
 
 
-// BAD : can select the same form twice
 void mainGame::random_select_forms(bool force)
 {
     // test if forms must be selected
@@ -129,13 +128,34 @@ void mainGame::random_select_forms(bool force)
         if(!empty) return;
     }
 
-    if(form_set.size() > 0) {
+    if(form_set.size() == 0) {
+        // empty set
+        for(size_t i=0; i<N_FORMS; i++) {
+            form[i] = (size_t) -1;
+        }
+    } else if(form_set.size() < N_FORMS) {
+        // set is too small to have no identic selected forms
         for(size_t i=0; i<N_FORMS; i++) {
             form[i] = rand() % form_set.size();
         }
     } else {
+        // set is big enough : never select the same form twice
+
+        /* algorithm : form_sorted contain the same values as form, sorted.
+         * each form slot is selected smaller than  max - i, which is exactly
+         * the number of value it can take (as 'i' are already taken).
+         * the inner loop transform  n -> the nth not taken value  while
+         * inserting it. */
+        size_t form_sorted[N_FORMS];
         for(size_t i=0; i<N_FORMS; i++) {
-            form[i] = (size_t) -1;
+            form[i] = rand() % (form_set.size() - i);
+            size_t j=i;
+            while(j>0 && form[i] >= form_sorted[j-1]) {
+                form_sorted[j] = form_sorted[j-1];
+                form[i]++;
+                j--;
+            }
+            form_sorted[j] = form[i];
         }
     }
 }
